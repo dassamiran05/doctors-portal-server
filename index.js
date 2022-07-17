510d5871443027968bde60509204c7cd807cf0e4
Samiran Das <dassamiran05@gmail.com>
2022-07-18 00:14:23 +0530

    Admin level work for server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const {
   MongoClient,
   ServerApiVersion
@@ -11,6 +12,22 @@ const port = process.env.PORT || 5000;
 //Midddleware
 app.use(cors());
 app.use(express.json());
+
+function verifyjwt(req, res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send({message:'Unauthorized Access'});
+  }
+  const token = authHeader.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function(err, decoded){
+    if(err){
+      return res.status(403).send({message:'Forbidden Access'});
+    }
+    req.decoded = decoded;
+    //console.log(decoded);
+    next();
+  });
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.t4diz.mongodb.net/?retryWrites=true&w=majority`;
@@ -26,6 +43,7 @@ async function run() {
     await client.connect();
     const servicesCollection = client.db('doctors_portal').collection('services');
     const bookingCollection = client.db('doctors_portal').collection('bookings');
+    const userCollection = client.db('doctors_portal').collection('users');
 
 
     /** 
@@ -38,13 +56,60 @@ async function run() {
      *  
      */
 
+    // Make Admin
+    app.put('/user/admin/:email', verifyjwt, async(req, res) =>{
+      const email = req.params.email;
+      const requester = req.decoded.email;
+      const requesterAccount = await userCollection.findOne({email:requester});
+      if(requesterAccount.role === 'admin'){
+        const filter = {email: email};
+        const updateDoc = {
+          $set: {role:'admin'}
+        };
 
+        const result = await userCollection.updateOne(filter, updateDoc);
+        res.send(result);
+      }
+    });
+
+    app.get('/admin/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = await userCollection.findOne({email: email});
+      const isAdmin = user.role === 'admin';
+      res.send({admin: isAdmin});
+      
+    });
+
+
+    app.put('/user/:email', async(req, res) =>{
+      const email = req.params.email;
+      const user = req.body;
+      const filter = {email: email};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user
+      };
+
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({email:email},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1h'});
+      res.send({result, token});
+    });
+
+    app.get('/users',verifyjwt, async(req, res) => {
+      const users = await userCollection.find().toArray();
+      res.send(users);
+    })
     
-    app.get('/booking', async(req, res) =>{
+    app.get('/booking',verifyjwt, async(req, res) =>{
       const patient = req.query.patient;
-      const query = {patient: patient};
-      const bookings = await bookingCollection.find(query).toArray();
-      res.send(bookings);
+      const decodedEmail = req.decoded.email;
+      if(patient === decodedEmail){
+        const query = {patient: patient};
+        const bookings = await bookingCollection.find(query).toArray();
+        res.send(bookings);
+      }else{
+        return res.status(403).send({message:'Forbidden Access'});
+      }
     })
 
 
